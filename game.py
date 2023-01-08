cba7fcf0503e72b89247246b9b6964cdeb343f73
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-01-08 13:25:02 +0100

    Load a random donut

--- game.py
+++ game.py
@@ -1,5 +1,13 @@
 import pygame
 from sys import exit
+from random import randint
+
+
+def draw_donut():
+    number = randint(1, 6)
+    random_donut = 'donut' + str(number) + '.png'
+    return random_donut
+
 
 pygame.init()
 screen_size = width, height = 900, 563
@@ -7,6 +15,7 @@ screen = pygame.display.set_mode((width, height))
 screen.fill((222, 238, 235))
 pygame.display.set_caption('SNAKE')
 clock = pygame.time.Clock()
+donut = pygame.image.load(f'donuts/{draw_donut()}').convert_alpha()
 
 while True:
     for event in pygame.event.get():
