0e35a749454913287adee1c4cded7e20b22bbd5b
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2023-01-08 13:14:20 +0100

    Create a main screen

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
 import pygame
+from sys import exit
+
+pygame.init()
+screen_size = width, height = 900, 563
+screen = pygame.display.set_mode((width, height))
+screen.fill((222, 238, 235))
+pygame.display.set_caption('SNAKE')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
